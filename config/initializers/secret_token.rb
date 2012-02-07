804f7f8b3d04a4aebd09f33170861df07e15125d
Deepak Ramamurthi <scdeepak@gmail.com>
2012-02-07 06:29:53 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anmollabs::Application.config.secret_token = '505e04198959121e0a540c779ee11bfef5fdd34df5272b1304522412c8c1105f826755f69f2801b62e75cc8d9edcd8d7dacc254c22d2ff86ba0e3140fe99b91b'
